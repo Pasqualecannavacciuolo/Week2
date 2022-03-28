00c79714f0437e169225a467ed6bbbb840257ae6
Pasquale <p.cannavacciuolo@icloud.com>
2022-03-28 14:30:24 +0200

    Added initial version

---
+++ src/main/java/DBConstant.java
@@ -0,0 +1,6 @@
+public class DBConstant {
+    public static final String DB_USER = "root";
+    public static final String DB_PASS = "Toor123@";
+    public static final String DB_URL = "jdbc:mysql://localhost/JDBC?";
+    public static final String DB_MYSQL_URL = "com.mysql.cj.jdbc.Driver";
+}
