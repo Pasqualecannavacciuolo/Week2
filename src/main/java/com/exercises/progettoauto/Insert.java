9c8463e4451f190ad62bab57cc280f2687b669a0
Pasquale <p.cannavacciuolo@icloud.com>
2022-03-31 08:49:02 +0200

    Closing the Insert class

--- src/main/java/com/exercises/progettoauto/Insert.java
+++ src/main/java/com/exercises/progettoauto/Insert.java
@@ -20,33 +20,33 @@ public class Insert extends DBOperations {
     @Override
     void operation() throws SQLException {
         String sql = "INSERT INTO `JDBC`.`Auto`(`Marchio`, `Nazione`, `Fatturato`, `Dipendenti`) VALUES (?,?,?,?);";
-
+        int nInsert;
         try {
-            System.out.print("\nInserisci nome marchio: ");
-            _nome = input.next();
-            System.out.print("\nInserisci nazione: ");
-            _nazione = input.next();
-            System.out.print("\nInserisci fatturato: ");
-            _fatturato = input.nextInt();
-            System.out.print("\nInserisci numero dipendenti: ");
-            _dipendenti = input.nextInt();
+            System.out.print("Quanti inserimenti vuoi effettuare?: ");
+            nInsert = input.nextInt();
+            for(int i=0; i<nInsert; i++) {
+                System.out.print("\nInserisci nome marchio: ");
+                _nome = input.next();
+                System.out.print("\nInserisci nazione: ");
+                _nazione = input.next();
+                System.out.print("\nInserisci fatturato: ");
+                _fatturato = input.nextInt();
+                System.out.print("\nInserisci numero dipendenti: ");
+                _dipendenti = input.nextInt();
 
-            ps = singleton.getConnection().prepareStatement(sql);
-            ps.setString(1, _nome);
-            ps.setString(2, _nazione);
-            ps.setInt(3, _fatturato);
-            ps.setInt(4, _dipendenti);
+                ps = singleton.getConnection().prepareStatement(sql);
+                ps.setString(1, _nome);
+                ps.setString(2, _nazione);
+                ps.setInt(3, _fatturato);
+                ps.setInt(4, _dipendenti);
 
-            if (ps.executeUpdate() != 0) L.info("Aggiunto ");
-            else L.info("non aggiunto");
+                if (ps.executeUpdate() != 0) {
+                    L.info("Aggiunto ");
+                    ps.clearParameters();
+                } else L.info("non aggiunto");
+            }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
+        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         } finally {
             singleton.closeConnection(this.connection, this.statement);
