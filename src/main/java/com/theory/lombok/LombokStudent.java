81fef6ec5f2d459b6b3efe58ab7ee2f8c396eb99
Pasquale <p.cannavacciuolo@icloud.com>
2022-03-31 11:37:43 +0200

    Minor changes

--- src/main/java/com/theory/lombok/LombokStudent.java
+++ src/main/java/com/theory/lombok/LombokStudent.java
@@ -1,2 +1,12 @@
-package com.theory.lombok;public class LombokStudent {
+package com.theory.lombok;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data @Builder
+public class LombokStudent {
+    private String name, lastname, email, username, password;
+    private int age;
+    private boolean enabled;
 }
